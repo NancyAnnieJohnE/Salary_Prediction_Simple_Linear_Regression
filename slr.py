16a2a9c385f8b8c575ff4fe57b18e857c5006fce
Nancy Annie John E <nancyannie.jo@gmail.com>
2026-06-26 15:42:52 +0530

    Add files via upload

---
+++ slr.py
@@ -0,0 +1,69 @@
+import numpy as nm  
+
+import matplotlib.pyplot as mtp  
+import pandas as pd  
+
+data_set= pd.read_csv('salary_data.csv')  
+print(data_set.head())
+
+x= data_set.iloc[:, :-1].values  
+print("independent value")
+print (x)
+y= data_set.iloc[:, 1].values   
+print("dependent value")
+
+print(y) 
+
+
+# Splitting the dataset into training and test set.  
+from sklearn.model_selection import train_test_split  
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=0) 
+print()
+print("X_TRAIN VALUES")
+print(x_train)
+print(len(x_train))
+
+print()
+print("X_TEST VALUES")
+print(x_test)
+print(len(x_test))
+
+print()
+print("Y_TRAIN VALUES")
+print(y_train)
+
+print()
+print("Y_TEST VALUES")
+print(y_test)
+
+
+
+#Fitting the Simple Linear Regression model to the training dataset  
+from sklearn.linear_model import LinearRegression  
+regressor= LinearRegression()  
+regressor.fit(x_train, y_train)  
+
+#Prediction of Test and Training set result  
+y_pred= regressor.predict([[10]])  
+x_pred= regressor.predict(x_train) 
+
+print()
+print(y_pred)
+
+
+mtp.scatter(x_train, y_train, color="green")   
+mtp.plot(x_train, x_pred, color="red")    
+mtp.title("Salary vs Experience (Training Dataset)")  
+mtp.xlabel("Years of Experience")  
+mtp.ylabel("Salary(In Rupees)")  
+mtp.show()   
+
+
+
+#visualizing the Test set results  
+mtp.scatter(x_test, y_test, color="blue")   
+mtp.plot(x_train, x_pred, color="red")    
+mtp.title("Salary vs Experience (Test Dataset)")  
+mtp.xlabel("Years of Experience")  
+mtp.ylabel("Salary(In Rupees)")  
+mtp.show()  
